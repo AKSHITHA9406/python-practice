d461d9bdef0c52b242e9aaffdb0729429c58a7fe
Akshitha <kothapallyakshitha09@gmail.com>
2025-06-19 21:02:35 +0530

    Add files via upload
    determination of functions and use of return statement

---
+++ functions.py
@@ -0,0 +1,21 @@
+def say_hi():
+    print("hello user")
+
+print("oreo")
+say_hi()
+print("dora")
+
+def say_hello(name , age):
+    print("hello " + name + "you are " + age)
+
+say_hello("bunny ", "13")
+
+#if you remove the " " for the number u gotta add str() to age like str(age)
+
+#usage of return statement
+def cube(num):
+    return num*num*num
+
+print(cube(3))
+
+
