3529e4fc8b00f057e9fcbc8c2e4b74d92ded71c8
Akshitha <kothapallyakshitha09@gmail.com>
2025-06-19 21:29:33 +0530

    Add files via upload
    for loop determination and few example cases

---
+++ forloop.py
@@ -0,0 +1,20 @@
+for letter in "python":
+    print(letter)
+
+colours = ["pink", "blue", "red"]
+for colour in colours:
+    print(colour)
+for index in range(len(colours)):
+    print(colours[index])
+
+for index in range(10):
+    print(index)
+
+for num in range(3,8):
+    print (num)
+
+for index in range(5):
+    if index == 0:
+        print("first iteration")
+    else:
+        print("not first")
